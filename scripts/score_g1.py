5cc48aba83deac6cb93e0febc6f977441186a55a
Fadi Nammour <fadinammour95@gmail.com>
2021-04-28 23:28:13 +0200

    SCORE comaparison + radio dset script

--- scripts/score_g1.py
+++ scripts/score_g1.py
@@ -21,7 +21,32 @@ sys.path.insert(0, path_score)
 from score import score
 import numpy as np
 import matplotlib.pyplot as plt
+from cv2 import resize, INTER_AREA
 
+# Define function
+def downsample_im(input_im, output_dim):
+    """Downsample image.
+    Based on opencv function resize.
+    [doc](https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#void%20resize(InputArray%20src,%20OutputArray%20dst,%20Size%20dsize,%20double%20fx,%20double%20fy,%20int%20interpolation))
+    The input image is downsampled to the dimensions specified in `output_dim`.
+    The downsampling method is based on the `INTER_AREA` method.
+    See [tensorflow_doc](https://www.tensorflow.org/api_docs/cc/class/tensorflow/ops/resize-area)
+    Each output pixel is computed by first transforming the pixel's footprint
+    into the input tensor and then averaging the pixels that intersect the
+    footprint. An input pixel's contribution to the average is weighted by the
+    fraction of its area that intersects the footprint.
+    This is the same as OpenCV's INTER_AREA.
+    An explanation of the INTER_AREA method can be found in the next
+    [link](https://medium.com/@wenrudong/what-is-opencvs-inter-area-actually-doing-282a626a09b3).
+    This version should be consistent with the tensorflow one.
+    Parameters
+    ----------
+    input_im: np.ndarray (dim_x, dim_y)
+        input image
+    output_dim: int
+        Contains the dimension of the square output image.
+    """
+    return resize(input_im, (output_dim, output_dim), interpolation=INTER_AREA)
 
 # Load Data
 f = open(data_path+"cfht_batch.pkl", "rb")
@@ -30,15 +55,17 @@ f.close()
     
 n_batch = batch['psf_hst'].shape[0]
 # generate the psfs in the spatial domain
-shape = batch['inputs'].shape[1:]
-psf_tile_cfht = np.array([np.fft.ifftshift(np.fft.irfft2(p, s=shape)) for p in batch['psf_cfht']])
+shape = batch['inputs'].shape[-1]
+psf_tile_partial = np.array([c/h for c,h in zip(batch['psf_cfht'],batch['psf_hst'])])
+psf_tile_partial = np.array([downsample_im(np.fft.ifftshift(np.fft.irfft2(p)),shape) for p in psf_tile_partial])
+psf_tile_partial = np.array([p/p.sum() for p in psf_tile_partial])
 
 gals_obs = batch['inputs']
-psfs = psf_tile_cfht
+psfs = psf_tile_partial
 
 # Initiate instance of score
 # set the value of gamma
-g1 = score(gamma=1,rip=True,verbose=False)
+g1 = score(gamma=1,rip=False,verbose=False)
 
 
 # Run SCORE
